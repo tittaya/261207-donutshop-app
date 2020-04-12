c312f88335bfefcda27a2b3f08b3875c7eadc6df
Tittaya <tittaya2542@gmail.com>
2020-04-13 00:15:27 +0700

    Updates

---
+++ precache-manifest.c58bcd5cff8d95f87959713f0969a819.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d27db20b7ff7411bc6d7",
+    "url": "/261207-donutshop-app/static/css/main.20af5bb7.chunk.css"
+  },
+  {
+    "revision": "d27db20b7ff7411bc6d7",
+    "url": "/261207-donutshop-app/static/js/main.d27db20b.chunk.js"
+  },
+  {
+    "revision": "19ab67a23625f28b3d99",
+    "url": "/261207-donutshop-app/static/js/1.19ab67a2.chunk.js"
+  },
+  {
+    "revision": "248ff7a42141bff8bbec",
+    "url": "/261207-donutshop-app/static/js/runtime~main.248ff7a4.js"
+  },
+  {
+    "revision": "a5c9219328701c4612bd262c0edee775",
+    "url": "/261207-donutshop-app/static/media/donutlogo.a5c92193.png"
+  },
+  {
+    "revision": "7850e30f3daea6c5d22a77e487526dae",
+    "url": "/261207-donutshop-app/static/media/donutlogoB.7850e30f.png"
+  },
+  {
+    "revision": "1aa5792b7c3488efe35b786865dc3350",
+    "url": "/261207-donutshop-app/static/media/donutlogoV.1aa5792b.png"
+  },
+  {
+    "revision": "bb17f26b45694158e941bb3bdd57a588",
+    "url": "/261207-donutshop-app/index.html"
+  }
+];
